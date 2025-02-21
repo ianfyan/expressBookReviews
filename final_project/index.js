8823865a52e8dac8fb7217aa37f791cfecbad745
ianfyan <imamkurfian@gmail.com>
2025-02-20 23:12:14 -0500

    add get books api

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,20 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+  const token = req.session.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized: No token provided" });
+  }
+
+  jwt.verify(token, "fingerprint_customer", (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Unauthorized: Invalid token" });
+    }
+
+    req.user = decoded;
+    next();
+  });
 });
  
 const PORT =5000;
